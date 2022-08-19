a4c3829b59ddb978a9f11928400dc5c91528cc68
Joe <reszketo.robert@gmail.com>
2022-08-20 00:02:51 +0200

    solution

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,5 +1,8 @@
+const getSecondsOfThisYearUntilNow = () => {
+    const todayDate = Date.now();
+    const january1st = new Date(new Date(todayDate).getFullYear(), 0, 1);
+    const august31st = new Date(new Date(todayDate).getFullYear(), 7, 31);
+    return Math.round((august31st - january1st) / 1000)
+}
 
-
-
-
-// export default getSecondsOfThisYearUntilNow;
+export default getSecondsOfThisYearUntilNow;
